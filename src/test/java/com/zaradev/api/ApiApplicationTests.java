abbbbb2260207a52ec28016b4ad22b1c0fe410c5
Zahra Hamdane <hamdane.zahra@gmail.com>
2024-06-14 15:47:32 +0200

    Configurer et structurer l'API avec des packages

---
+++ src/test/java/com/zaradev/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zaradev.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
